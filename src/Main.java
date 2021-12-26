96587f175bcb01d5873bc9f47d84c92bffe1db88
Igor Berendejev <igor.berendejev@hotmail.com>
2021-12-27 00:42:07 +0200

    branch for checking

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Thread game = new Thread(new Game());
         game.start();
+        //adding branch for checking by SkillFactory
     }
 }
